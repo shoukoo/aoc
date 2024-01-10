bd268baf36250716e188b80ac4acaeb6df4a76c8
shoukoo <andyhsieh@duck.com>
2024-01-11 05:06:02 +1100

    2023: day1 cleanup code

--- 2023/01/01.zig
+++ 2023/01/01.zig
@@ -8,24 +8,22 @@ test "solution 1" {
     var i = split(u8, input.puzzle_input, "\n");
     var total: u32 = 0;
     while (i.next()) |l| {
-        // var first: ?u8 = null;
-        // var last: ?u8 = null;
-        print("slice {s}\n", .{l});
+        // Initialise the array with empty values. In this case, the null values are being used to initialise the ?u8 type
         var storage: [2]?u8 = std.mem.zeroes([2]?u8);
-        for (l) |c| {
-            if (c < '0' or c > '9') {
+        for (l) |char| {
+            // if the character is outside of 0 - 9 range then skip
+            if (char < '0' or char > '9') {
                 continue;
             }
             if (storage[0] == null) {
-                storage[0] = c;
+                storage[0] = char;
             }
-            storage[1] = c;
+            storage[1] = char;
         }
 
-        const hello = [2]u8{ storage[0].?, storage[1].? };
-        const num: u8 = std.fmt.parseInt(u8, &hello, 10) catch unreachable;
+        const num_string = [2]u8{ storage[0].?, storage[1].? };
+        const num: u8 = std.fmt.parseInt(u8, &num_string, 10) catch unreachable;
         total += num;
-        print("total {d}\n", .{total});
     }
 
     try expect(total == 55123);
