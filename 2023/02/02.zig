d4623afff52692ca313613f5c098ccf5a469649a
shoukoo <andyhsieh@duck.com>
2024-01-15 06:11:29 +1100

    2023: day2 wip solution 1

--- 2023/02/02.zig
+++ 2023/02/02.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
+const Cube = struct {
+    red: usize = 0,
+    green: usize = 0,
+    blue: usize = 0,
+};
+
 test "solution 1" {
     const file = try std.fs.cwd().openFile("input.txt", .{});
     defer file.close();
@@ -16,10 +22,21 @@ test "solution 1" {
 
     while (i.next()) |line| {
         std.debug.print("line {s}\n", .{line});
+        // use the colon to seperate the string literal
         if (std.mem.indexOf(u8, line, ":")) |value| {
-            std.debug.print("{s}\n", .{line[0..value]});
+            // use index of the colon to get the Game number
+            const space_sep = std.mem.indexOf(u8, line[0..value], " ").?;
+            const game_no_str = line[space_sep + 1 .. value];
+            const game_no = try std.fmt.parseInt(u8, game_no_str, 2);
+            std.debug.print("Game {d}\n", .{game_no});
             // value + 2 is to skip the colon and a space
             std.debug.print("{s:*^100}\n", .{line[value + 2 ..]});
+            var token = std.mem.tokenizeSequence(u8, line[value + 2 ..], "; ");
+
+            while (token.next()) |t| {
+                std.debug.print("{s}\n", .{t});
+            }
+
             break;
         }
     }
